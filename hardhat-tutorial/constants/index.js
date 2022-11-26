78cc0b1f480fa67cfd0f276a37a81be973ce0dfe
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-11-26 23:54:47 +0530

    completed

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xA205168067dAaCD75ab394f92Fc9FdF6a988ffee";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
